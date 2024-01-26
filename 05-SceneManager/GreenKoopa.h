4303e513b6f20dd647465e34b54aba0f5d347c6a
KhanhHa2708 <38288165+KhanhHa2708@users.noreply.github.com>
2024-01-27 05:03:25 +0700

    Add Green Koopa + Green Koopa Wing

--- 05-SceneManager/GreenKoopa.h
+++ 05-SceneManager/GreenKoopa.h
@@ -1,4 +1,3 @@
-
 #pragma once
 #pragma once
 #include "GameObject.h"
@@ -12,9 +11,9 @@
 #define GREEN_KOOPA_FLYING_SPEED 0.25f
 
 #define GREEN_KOOPA_BBOX_WIDTH 16
-#define GREEN_KOOPA_BBOX_HEIGHT 14
+#define GREEN_KOOPA_BBOX_HEIGHT 27
 #define GREEN_KOOPA_WING_BBOX_WIDTH 20
-#define GREEN_KOOPA_WING_BBOX_HEIGHT 24
+#define GREEN_KOOPA_WING_BBOX_HEIGHT 27
 #define GREEN_KOOPA_BBOX_HEIGHT_DIE 7
 
 #define GREEN_KOOPA_DIE_TIMEOUT 500
@@ -24,9 +23,12 @@
 #define GREEN_KOOPA_STATE_JUMPING 101
 #define GREEN_KOOPA_STATE_FLYING 102
 
-#define ID_ANI_GREEN_KOOPA_WALKING 5000
-#define ID_ANI_GREEN_KOOPA_DIE 5001
-#define ID_ANI_GREEN_KOOPA_WING_WALKING 5002
+#define ID_ANI_GREEN_KOOPA_WALKING_LEFT 7000
+#define ID_ANI_GREEN_KOOPA_WALKING_RIGHT 7001
+#define ID_ANI_GREEN_KOOPA_DIE 7002
+#define ID_ANI_GREEN_KOOPA_WING_WALKING_LEFT 7003
+#define ID_ANI_GREEN_KOOPA_WING_WALKING_RIGHT 7004
+
 
 #define GREEN_KOOPA_WALK_DURATION 2500
 #define GREEN_KOOPA_JUMP_DURATION 2000
@@ -40,8 +42,7 @@ protected:
 	float ax;
 	float ay;
 	int level;
-	int step;
-
+	
 	ULONGLONG die_start;
 	ULONGLONG timeEllapsed;
 
@@ -60,5 +61,5 @@ public:
 	void SetLevel(int l);
 	CGreenKoopa(float x, float y, int level = 0);
 	virtual void SetState(int state);
-	void CalcGoombaMove();
+	
 };
