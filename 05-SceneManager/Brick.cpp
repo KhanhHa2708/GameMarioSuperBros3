1609d79c61aa85592d83e7171ee00fe3089cc656
KhanhHa2708 <38288165+KhanhHa2708@users.noreply.github.com>
2024-01-27 05:27:35 +0700

    % P Switch

--- 05-SceneManager/Brick.cpp
+++ 05-SceneManager/Brick.cpp
@@ -4,13 +4,29 @@ void CBrick::Render()
 {
 	CAnimations* animations = CAnimations::GetInstance();
 	animations->Get(ID_ANI_BRICK)->Render(x, y);
+
 	//RenderBoundingBox();
 }
 
-void CBrick::GetBoundingBox(float& l, float& t, float& r, float& b)
+void CBrick::GetBoundingBox(float &l, float &t, float &r, float &b)
 {
-	l = x - BRICK_BBOX_WIDTH / 2;
-	t = y - BRICK_BBOX_HEIGHT / 2;
+	l = x - BRICK_BBOX_WIDTH/2;
+	t = y - BRICK_BBOX_HEIGHT/2;
 	r = l + BRICK_BBOX_WIDTH;
 	b = t + BRICK_BBOX_HEIGHT;
+}
+void CBrick::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
+{
+	
+
+	if (state == STATE_UPDATE)
+	{
+		isDeleted = true;
+		return;
+	}
+
+	
+
+	CGameObject::Update(dt, coObjects);
+	CCollision::GetInstance()->Process(this, dt, coObjects);
 }
